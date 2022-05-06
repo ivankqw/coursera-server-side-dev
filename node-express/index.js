ccf2927b8bb9ada8341356cef5c1a48ec22e4e68
ivan <ivankohquanwei@gmail.com>
2022-05-06 10:31:08 +0800

    express simple REST

--- node-express/index.js
+++ node-express/index.js
@@ -1,16 +1,75 @@
-const express = require('express'),
-     http = require('http');
-const morgan = require('morgan');
+const express = require("express"),
+  http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 
-app.use(morgan('dev'));
+//middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(__dirname + '/public'));
+//dishes endpoint, callback with req, res, next
+app.all("/dishes", (req, res, next) => {
+  //when a request comes in, status code set to 200 and set header
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  //continue on to look for specification that matches the dishes endpoint
+  //will work for GET, PUT etc
+  next();
+});
+
+//modification to res carried into get
+app.get("/dishes", (req, res, next) => {
+  res.end("Will send all the dishes to you!");
+});
+
+app.post("/dishes", (req, res, next) => {
+  res.end(
+    "Will add the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+
+app.put("/dishes", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("PUT operation not supported on /dishes");
+});
+
+//will see how we can add authentication later
+app.delete("/dishes", (req, res, next) => {
+  res.end("Deleting all dishes");
+});
+
+//more specific
+app.get("/dishes/:dishId", (req, res, next) => {
+  res.end("Will send details of the dish: " + req.params.dishId + " to you!");
+});
+
+app.post("/dishes/:dishId", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("POST operation not supported on /dishes/" + req.params.dishId);
+});
+
+app.put("/dishes/:dishId", (req, res, next) => {
+  res.write("Updating the dish: " + req.params.dishId + "\n");
+  res.end(
+    "Will update the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+
+app.delete("/dishes/:dishId", (req, res, next) => {
+  res.end("Deleting dish: " + req.params.dishId);
+});
 
 const server = http.createServer(app);
 
