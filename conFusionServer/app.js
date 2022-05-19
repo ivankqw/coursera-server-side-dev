96ffc55bd7020b85b96b4e9f30554ad0ad75d737
ivan <ivankohquanwei@gmail.com>
2022-05-19 10:41:16 +0800

    Express Session Part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -28,6 +28,29 @@ connect.then(
 
 var app = express();
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+    console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
+  }
+}
+
 //using express session cookies with file storage
 app.use(
   session({
